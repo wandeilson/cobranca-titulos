f0714b56a39d244b5596f67201304ba5fa8f57ba
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-01-12 22:02:04 -0300

    Adicionando campo STATUS dinamicamente

--- src/main/java/com/wandeilson/cobranca/controller/TituloController.java
+++ src/main/java/com/wandeilson/cobranca/controller/TituloController.java
@@ -1,11 +1,17 @@
 package com.wandeilson.cobranca.controller;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.wandeilson.cobranca.model.StatusTitulo;
 import com.wandeilson.cobranca.model.Titulo;
 import com.wandeilson.cobranca.repository.Titulos;
 
@@ -19,15 +25,25 @@ public class TituloController {
 	
 
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
 	}
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView salvar(Titulo titulo) {
-		System.out.println(">>>"+ titulo.getDescricao());
 		titulos.save(titulo);
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem","Título salvo com sucesso!");
 		return mv;
 	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+	}
+	
+	
+	
+	
 }
