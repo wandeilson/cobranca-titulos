387d98191e8eae59647c50838faaead77972a7e0
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-01-14 16:46:40 -0300

    Matem valores no form

--- src/main/java/com/wandeilson/cobranca/controller/TituloController.java
+++ src/main/java/com/wandeilson/cobranca/controller/TituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.wandeilson.cobranca.model.StatusTitulo;
 import com.wandeilson.cobranca.model.Titulo;
@@ -34,14 +35,13 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if(errors.hasErrors()) {
-			return mv;
+			return "CadastroTitulo";
 		}
 		titulos.save(titulo);
-		mv.addObject("mensagem","Título salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem","Título salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 	@RequestMapping
 	public ModelAndView pesquisar() {
