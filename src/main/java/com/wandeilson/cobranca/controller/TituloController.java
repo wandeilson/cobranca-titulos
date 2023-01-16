2325a35bd4d21a439914303f5c49744035a52861
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-01-16 16:28:37 -0300

    Adiciona calendário e toolkit

--- src/main/java/com/wandeilson/cobranca/controller/TituloController.java
+++ src/main/java/com/wandeilson/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -43,9 +44,14 @@ public class TituloController {
 		if(errors.hasErrors()) {
 			return CADASTRO_VIEW;
 		}
-		titulos.save(titulo);
-		attributes.addFlashAttribute("mensagem","Título salvo com sucesso!");
-		return "redirect:/titulos/novo";
+		try {
+			titulos.save(titulo);
+			attributes.addFlashAttribute("mensagem","Título salvo com sucesso!");
+			return "redirect:/titulos/novo";
+		}catch(DataIntegrityViolationException e) {
+			errors.rejectValue("dataVencimento", null,"Formato de data inválido");
+			return CADASTRO_VIEW;
+		}
 	}
 	@RequestMapping
 	public ModelAndView pesquisar() {
