6d38d2855e09cdbe2f9b21897fe785b6aefc3ea3
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-01-16 16:54:39 -0300

    Adiciona maskMoney

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -17,4 +17,5 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event){
 
 $(function(){
 	$('[rel="tooltip"]').tooltip();
+	$('.js-currency').maskMoney({decimal:',', thousands:'.', allowZero: true});
 });
